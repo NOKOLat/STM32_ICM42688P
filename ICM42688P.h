43ed3506f4e17dad3915ff217b8b59806fc3ab94
Kuboki <s247848x@st.go.tuat.ac.jp>
2025-05-02 15:16:38 +0900

    Update ICM42688P.h

--- ICM42688P.h
+++ ICM42688P.h
@@ -135,7 +135,7 @@ class ICM42688P{
         // Offset
         int16_t accel_offset[3] = {};
         int16_t gyro_offset[3] = {};
-	float AccelGain = 1;
+	float accel_gain = 0.0;
 
         // Config
         float accel_scale_value = 0.0;
